2559203e079e595120aa82d9ee951e2150c0f091
EminenceGrise <eminencegrs@gmail.com>
2015-07-03 22:00:57 +0300

    New web project

---
+++ ArgamakHorses/App_Start/PreStartApp.cs
@@ -0,0 +1,12 @@
+﻿namespace ArgamakHorses.App_Start
+{
+    public static class PreStartApp
+    {
+        private static NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public static void Start()
+        {
+            Logger.Info("Pre-Start Application");
+        }
+    }
+}
